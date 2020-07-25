b2146825e8110ff35dcc62fe560946a50966cd31
AthanasiosDelis <th.delis@gmail.com>
2020-07-25 15:42:25 +0300

    A ton of experiments,whiles,keys,for,functions
    Unfinished bussines:
    Find a way to press Enter, for changing the state of a while loop, with focus on the document

--- JAVASCRIPT_SECOND_EXPERIMENT/home.js
+++ JAVASCRIPT_SECOND_EXPERIMENT/home.js
@@ -1,8 +1,50 @@
-console.log("Hello Word!");
-console.log("This is Thanos");
-alert("Hello Word!")
-var b='var_b';
-console.log(b);
+//console.log("Hello Word!");
+//console.log("This is Thanos");
+//alert("Hello Word!")
+//var b='var_b';
+//console.log(b);
 
-var age=prompt('What is your age?')
-document.getElementById('someText').innerHTML=age;
+//var age=prompt('What is your age?')
+//document.getElementById('someText').innerHTML=age;
+//function funct(){
+//    alert('this is a function');
+//}
+//while test\
+
+//var n=3;
+/*while(n<5)
+{
+   
+    document.querySelector('input').addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+          console.log('You pressed Enter');
+          n=100;
+        }
+      })
+      n=prompt('Give permision!');
+        
+}*/
+
+    //onclick="javascript:var n=100; javascript:alet(n);"
+//callin funct
+//funct();
+/*
+function name(){
+    var name=prompt('What is your name?');
+    var message='Hello my friend '+ name+'.\n';
+    m2= 'I learnt your age is '+age+'!';
+    console.log(age);
+    console.log(message ,m2);
+    document.getElementById('someText').innerHTML=m2;
+}
+name();
+*/
+let cars=['BMW','TESLA','FORD','FERRARI','TOYOTA'];
+console.log(cars[1]);
+for (let i; i<cars.legth; i++){
+    console.log(crs[i]);
+}
+console.log('to string', cars.toString);
+console.log(cars.join('*'));
+console.log(cars,cars.pop('TESLA'));
+console.log(cars,cars.push("LAMBO"));
