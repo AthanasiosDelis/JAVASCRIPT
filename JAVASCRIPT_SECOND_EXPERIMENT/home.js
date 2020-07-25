63973a70ed20f5d63ccaad5b9a2167f3551a0c4a
AthanasiosDelis <th.delis@gmail.com>
2020-07-25 14:08:40 +0300

    Pop ups UI began! age question.

---
+++ JAVASCRIPT_SECOND_EXPERIMENT/home.js
@@ -0,0 +1,8 @@
+console.log("Hello Word!");
+console.log("This is Thanos");
+alert("Hello Word!")
+var b='var_b';
+console.log(b);
+
+var age=prompt('What is your age?')
+document.getElementById('someText').innerHTML=age;
