76d8f486645758589335e3f6d50af378dc9987f7
AthanasiosDelis <th.delis@gmail.com>
2020-07-25 16:59:57 +0300

    trying to figure out JSON edition
    All previous work in comment, most work accomplished rests inside the home.js file

--- JAVASCRIPT_SECOND_EXPERIMENT/home.js
+++ JAVASCRIPT_SECOND_EXPERIMENT/home.js
@@ -4,7 +4,7 @@
 //var b='var_b';
 //console.log(b);
 
-//var age=prompt('What is your age?')
+var age=prompt('What is your age?')
 //document.getElementById('someText').innerHTML=age;
 //function funct(){
 //    alert('this is a function');
@@ -40,11 +40,62 @@ function name(){
 name();
 */
 let cars=['BMW','TESLA','FORD','FERRARI','TOYOTA'];
-console.log(cars[1]);
+//console.log(cars[1]);
 for (let i; i<cars.legth; i++){
     console.log(crs[i]);
 }
-console.log('to string', cars.toString);
-console.log(cars.join('*'));
-console.log(cars,cars.pop('TESLA'));
-console.log(cars,cars.push("LAMBO"));
+//console.log('to string', cars.toString);
+//console.log(cars.join('*'));
+//console.log(cars,cars.pop('TESLA'));
+const index = cars.indexOf('TESLA');
+if (index > -1) {
+  cars.splice(index, 1);
+}
+//console.log(cars);
+//console.log(cars,cars.push("LAMBO"));
+
+
+let planes=['airbus','boeing','lockheed'];
+let allvahiclesmarks=cars.concat(planes);
+//console.log(allvahiclesmarks);
+//console.log(allvahiclesmarks.slice(1,4));
+//console.log(allvahiclesmarks.reverse());
+let randnum=Array(10).fill().map(() => Math.round(Math.random() * 40));
+console.log(randnum);
+console.log(randnum.sort(function(a,b){return a-b}));
+console.log(randnum.sort(function(a,b){return b-a}));
+let emptyArray=new Array();
+for(let num=0; num<=10; num++){
+    emptyArray.push(num);
+}
+console.log(emptyArray);
+let student={first:'Delis',last:'Athanasios',age:25,height:170,
+studentInfo: function(){
+    return this.first+'\n'+this.last;+'\n'+this.age;
+}
+};
+//console.log(student.first);
+//console.log(student.last);
+console.log(student.studentInfo());
+if((age>=18 &&(age<=35))){
+    status='target age group';
+    console.log(status);
+}else{
+    status='indiffent group';
+    console.log(status);
+}
+switch(6){
+    case 0:
+        text='Sunday';
+        break;
+    case 1:
+        text='Monday';
+        break;
+    case 6:
+        text='Saturday';
+        break;
+    default:
+        text='Everything begins on Mondays';
+        break;
+}
+console.log(text);
