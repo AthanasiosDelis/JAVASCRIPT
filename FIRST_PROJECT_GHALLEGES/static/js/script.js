49992cc7e61a87a99c979b0d6617bc870f2e2543
AthanasiosDelis <th.delis@gmail.com>
2020-07-27 14:04:56 +0300

    funny change for a friend
    added a check for the year

--- FIRST_PROJECT_GHALLEGES/static/js/script.js
+++ FIRST_PROJECT_GHALLEGES/static/js/script.js
@@ -1,18 +1,29 @@
 //alert("Let's commense");]
 function ageInDays(){
+//how do i initiallize eveything?    
     if(document.getElementById('ageInDays')!=null){
     document.getElementById('ageInDays').remove();
     }
+    //main fuctionality
     var birthYear=prompt('What is the year of your birth?');
+    var oldest=false;
+    console.log(oldest);
+    if(birthYear<=1900){
+        oldest=true;
+        console.log(oldest);
+    }
     var ageInDayss=(2020-birthYear)*365;
     h1=document.createElement('h1');
-    var textAnswer=document.createTextNode('You are '+ ageInDayss+' days old');
+    var textAnswer;
+    if(oldest){
+        textAnswer=document.createTextNode('You are '+ ageInDayss+' days old, dude! Oldest person alive!! Why are you still alive?');
+    }
+    else{
+        textAnswer=document.createTextNode('You are '+ ageInDayss+' days old');
+    }
     h1.setAttribute('id','ageInDays');
     h1.appendChild(textAnswer);
     document.getElementById('flex-box-result').appendChild(h1);    //console.log(ageInDayss)
-//how do i initiallize eveything?
-    h1=null;
-    textAnswer=null;
 }
 function reset(){
     document.getElementById('ageInDays').remove();
